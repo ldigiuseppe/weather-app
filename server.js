a48a385f9e3ad72e5585588da646ed20c6e91043
Luciano Di Giuseppe <luciano.digiuseppe@heyday.co.nz>
2020-05-30 14:29:30 +1200

    first commit, added server and some basic stuff

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+})
+
+app.listen(3000, function () {
+  console.log('App running on port 3000')
+})
+
+
