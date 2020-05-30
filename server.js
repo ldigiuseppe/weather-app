2d8b7fe57d701ea32f96592497adb13e2bdc2514
Luciano Di Giuseppe <luciano.digiuseppe@heyday.co.nz>
2020-05-30 15:37:57 +1200

    Added env file with config, added nodemon for dev

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const request = require('request');
+const dotenv = require('dotenv');
 const app = express()
+
+dotenv.config();
+
+const weatherApiKey = process.env.WEATHER_API_KEY;
+const port = process.env.PORT;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city);
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
-app.listen(3000, function () {
-  console.log('App running on port 3000')
+app.listen(port, function () {
+  console.log(`App running on port ${port}`);
 })
-
 
